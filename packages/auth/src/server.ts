28995bbbc375f48651ff98a65301b875c5eca0a3
Lukas Thomsen <195757696+lukasthomsen@users.noreply.github.com>
2026-05-23 07:58:18 +0200

    Prompt Google account selection

--- packages/auth/src/server.ts
+++ packages/auth/src/server.ts
@@ -478,6 +478,7 @@ async function buildSocialProviders() {
     socialProviders.google = {
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
+      prompt: "select_account",
     };
   }
 
